e276a1baf1bea50220f7b9167e286b570c86980f
non1463 <loiscc13@gmail.com>
2024-02-03 19:41:12 +0100

    perf_t function to test time perfs

--- src/main.cpp
+++ src/main.cpp
@@ -16,12 +16,46 @@ char get_gradient_char(char* rgba)
     return gradient[((u8)rgba[0] + (u8)rgba[1] + (u8)rgba[2]) * (u8)rgba[3] * 8u / (256u*256u*3u)];
 }
 
+void perf_t(string format)
+{
+    char* bytes_;
+    double diff;
+    time_t start_t;
+    if (format == "QOI")
+    {
+        // read
+        start_t = time(nullptr);
+        for (u32 i = 0; i< 10000u; i++)
+        {
+            QOI::Read("../images/test.qoi", header, bytes_);
+            delete[] bytes_;
+        }
+        diff = difftime(time(nullptr), start_t);
+        cout << "read perf : " << diff/10000. << "s\n";
+
+        //write
+        QOI::Read("../images/test.qoi", header, bytes_);
+        start_t = time(nullptr);
+        for (u32 i = 0; i< 10000u; i++)
+        {
+            QOI::Write("../images/test.qoi", header, bytes_);
+        }
+        diff = difftime(time(nullptr), start_t);
+        cout << "write perf : " << diff/10000. << "s\n";
+    }
+
+
+    delete[] bytes_;
+}
+
 int main()
 {
 
     QOI::Read("../images/dice.qoi",header, bytes);
     QOI::Write("../images/test.qoi", header, bytes);
-    QOI::Read("../images/test.qoi", header, bytes);
+
+
+    perf_t("QOI");
 
 
     std::ofstream file("../images/test.txt");
